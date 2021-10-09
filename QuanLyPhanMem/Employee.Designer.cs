3c5a7d44ad7a1d704c80ce6736d8bf18b86c2641
Sioldil <61321409+Sioldil@users.noreply.github.com>
2021-10-09 19:58:35 +0700

    DSNV
    mới tạo

---
+++ QuanLyPhanMem/Employee.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace QuanLyPhanMem
+{
+    partial class Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.qLDADataSetEmployee = new QuanLyPhanMem.QLDADataSetEmployee();
+            this.nhanvienBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nhanvienTableAdapter = new QuanLyPhanMem.QLDADataSetEmployeeTableAdapters.NhanvienTableAdapter();
+            this.colMaNV = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTenNV = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colGioiTinh = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSDT = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNgaysinh = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colChucvu = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colMail = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLDADataSetEmployee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhanvienBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.nhanvienBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(800, 450);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colMaNV,
+            this.colTenNV,
+            this.colGioiTinh,
+            this.colSDT,
+            this.colNgaysinh,
+            this.colChucvu,
+            this.colMail});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // qLDADataSetEmployee
+            // 
+            this.qLDADataSetEmployee.DataSetName = "QLDADataSetEmployee";
+            this.qLDADataSetEmployee.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // nhanvienBindingSource
+            // 
+            this.nhanvienBindingSource.DataMember = "Nhanvien";
+            this.nhanvienBindingSource.DataSource = this.qLDADataSetEmployee;
+            // 
+            // nhanvienTableAdapter
+            // 
+            this.nhanvienTableAdapter.ClearBeforeFill = true;
+            // 
+            // colMaNV
+            // 
+            this.colMaNV.FieldName = "MaNV";
+            this.colMaNV.Name = "colMaNV";
+            this.colMaNV.Visible = true;
+            this.colMaNV.VisibleIndex = 0;
+            // 
+            // colTenNV
+            // 
+            this.colTenNV.FieldName = "TenNV";
+            this.colTenNV.Name = "colTenNV";
+            this.colTenNV.Visible = true;
+            this.colTenNV.VisibleIndex = 1;
+            // 
+            // colGioiTinh
+            // 
+            this.colGioiTinh.FieldName = "GioiTinh";
+            this.colGioiTinh.Name = "colGioiTinh";
+            this.colGioiTinh.Visible = true;
+            this.colGioiTinh.VisibleIndex = 2;
+            // 
+            // colSDT
+            // 
+            this.colSDT.FieldName = "SDT";
+            this.colSDT.Name = "colSDT";
+            this.colSDT.Visible = true;
+            this.colSDT.VisibleIndex = 3;
+            // 
+            // colNgaysinh
+            // 
+            this.colNgaysinh.FieldName = "Ngaysinh";
+            this.colNgaysinh.Name = "colNgaysinh";
+            this.colNgaysinh.Visible = true;
+            this.colNgaysinh.VisibleIndex = 4;
+            // 
+            // colChucvu
+            // 
+            this.colChucvu.FieldName = "Chucvu";
+            this.colChucvu.Name = "colChucvu";
+            this.colChucvu.Visible = true;
+            this.colChucvu.VisibleIndex = 5;
+            // 
+            // colMail
+            // 
+            this.colMail.FieldName = "Mail";
+            this.colMail.Name = "colMail";
+            this.colMail.Visible = true;
+            this.colMail.VisibleIndex = 6;
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Employee";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Employee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLDADataSetEmployee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhanvienBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private QLDADataSetEmployee qLDADataSetEmployee;
+        private System.Windows.Forms.BindingSource nhanvienBindingSource;
+        private QLDADataSetEmployeeTableAdapters.NhanvienTableAdapter nhanvienTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colMaNV;
+        private DevExpress.XtraGrid.Columns.GridColumn colTenNV;
+        private DevExpress.XtraGrid.Columns.GridColumn colGioiTinh;
+        private DevExpress.XtraGrid.Columns.GridColumn colSDT;
+        private DevExpress.XtraGrid.Columns.GridColumn colNgaysinh;
+        private DevExpress.XtraGrid.Columns.GridColumn colChucvu;
+        private DevExpress.XtraGrid.Columns.GridColumn colMail;
+    }
+}
